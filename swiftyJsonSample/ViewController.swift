9734f2eb63cfcf7f6b4a40e5a76e51357f538747
User <user@kolamazz-macbook.local>
2019-02-19 18:08:34 +0530

    First commit

---
+++ swiftyJsonSample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  swiftyJsonSample
+//
+//  Created by User on 19/02/19.
+//  Copyright © 2019 Alisons Infomatics. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Alamofire.request("http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+
+            }
+
+        }
+    }
+
+
+
+
+    
+}
