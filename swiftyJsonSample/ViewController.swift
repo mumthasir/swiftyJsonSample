c189645ecd428a62d570b64b8326c61687a6f1c5
User <user@kolamazz-macbook.local>
2019-02-19 18:09:50 +0530

    comment commit

--- swiftyJsonSample/ViewController.swift
+++ swiftyJsonSample/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
             if((responseData.result.value) != nil) {
                 let swiftyJsonVar = JSON(responseData.result.value!)
 
+
+                //Commented
             }
 
         }
@@ -27,5 +29,5 @@ class ViewController: UIViewController {
 
 
 
-    
+
 }
